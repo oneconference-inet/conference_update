3b16447d613a48bd7aefb04a7662cd7df35907e5
tlests <sutthisak.so@inet.co.th>
2021-02-03 22:16:43 +0700

    no message

--- react/features/base/participants/reducer.js
+++ react/features/base/participants/reducer.js
@@ -85,6 +85,7 @@ ReducerRegistry.register("features/base/participants", (state = [], action) => {
             // the app and "leaves" at the end of the app).
             const { conference, id } = action.participant;
 
+            console.log("infoUser.getUserId(): ",infoUser.getUserId());
             const socket = socketIOClient(interfaceConfig.DOMAIN);
             socket.emit("left", {
                 status: "left",
