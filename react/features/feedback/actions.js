0f5f1f13c254090376e67f05707fbdbaca4318c1
tlests <sutthisak.so@inet.co.th>
2021-03-11 14:16:42 +0700

    :zap: fix getservice()

--- react/features/feedback/actions.js
+++ react/features/feedback/actions.js
@@ -33,7 +33,7 @@ import axios from "axios";
  */
 export function cancelFeedback(score: number, message: string) {
     window.location.href =
-        infoConf.getIsHostHangup() && getService() !== ""
+        infoConf.getIsHostHangup() && infoConf.getService() === ""
             ? interfaceConfig.DOMAIN + "/main"
             : infoUser.getRedirect();
     return {
@@ -147,7 +147,7 @@ export function submitFeedback(
         .then(
             (res) =>
                 (window.location.href =
-                    infoConf.getIsHostHangup() && getService() !== ""
+                    infoConf.getIsHostHangup() && infoConf.getService() === ""
                         ? interfaceConfig.DOMAIN + "/main"
                         : infoUser.getRedirect())
         );
