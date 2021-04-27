00ef64cbfb5fc02baf33e826082a271f14dc21eb
Aphinet Damkra <62979677+gitaphinet@users.noreply.github.com>
2021-04-27 11:58:02 +0700

    :tr: ..

--- react/features/remote-video-menu/components/web/EndMeetingDialog.js
+++ react/features/remote-video-menu/components/web/EndMeetingDialog.js
@@ -102,10 +102,10 @@ class EndMeetingDialog extends AbstractEndMeetingParticipantDialog<Props> {
 
             if (service == "onechat") {
                 domainEnd = interfaceConfig.DOMAIN_BACK + '/service/endmeeting'
-                await axios.post(domainEnd, { meetingid : infoConf.getMeetingId(), tag: service }, {headers: { Authorization: "Bearer "+ secretKeyOnechat }})
+                await axios.post(domainEnd, { meetingid : infoConf.getMeetingId(), tag: service }, {headers: { Authorization: "Bearer " + secretKeyOnechat }})
             } else if (service == "manageAi") {
                 domainEnd = interfaceConfig.DOMAIN_BACK + '/service/endmeeting'
-                await axios.post(domainEnd, { meetingid : infoConf.getMeetingId(), tag: service }, {headers: { Authorization: "Bearer "+ secretKeyManageAi }})
+                await axios.post(domainEnd, { meetingid : infoConf.getMeetingId(), tag: service }, {headers: { Authorization: "Bearer " + secretKeyManageAi }})
                 console.log('------------------secretKeyManageAi------------------------', secretKeyManageAi);
             } else if (service == "onemail") {
                 domainEnd = interfaceConfig.DOMAIN_ONEMAIL + '/api/v1/oneconf/service/endmeeting'
