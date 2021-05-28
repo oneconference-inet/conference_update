81c14c1b937dce10413a166a8d8e9129ac534462
MaMookMik <mookjrtv3@gmail.com>
2021-05-28 16:12:14 +0700

    test

--- react/features/polls/components/AbstractPollAnswerDialog.js
+++ react/features/polls/components/AbstractPollAnswerDialog.js
@@ -53,6 +53,7 @@ const AbstractPollAnswerDialog = (Component: AbstractComponent<AbstractProps>) =
         // newCheckBoxStates[index] = state;
         const newCheckBoxStates = checkBoxStates.map((ind, checkBox) => {
             checkBox = ind === index ? state : false
+            console.log('checkbox = ', checkBox, ind);
             return checkBox
         })
         setCheckBoxState(newCheckBoxStates);
