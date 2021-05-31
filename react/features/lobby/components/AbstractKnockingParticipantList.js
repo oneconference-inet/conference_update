a5fdd795aced319cde5a6cb450f9be288a391bf8
tlests <tlests@hotmail.com>
2021-05-31 12:16:28 +0700

    delete log

--- react/features/lobby/components/AbstractKnockingParticipantList.js
+++ react/features/lobby/components/AbstractKnockingParticipantList.js
@@ -93,9 +93,9 @@ export default class AbstractKnockingParticipantList<P: Props = Props> extends P
  */
 export function mapStateToProps(state: Object): $Shape<Props> {
     const { knockingParticipants } = state['features/lobby'];
-    console.log("isLocalParticipantModerator: ",isLocalParticipantModerator(state));
+    
     return {
         _participants: knockingParticipants,
-        _visible: Boolean(knockingParticipants.length)
+        _visible: isLocalParticipantModerator(state) && Boolean(knockingParticipants.length)
     };
 }
