3d22338b1ca64216013b5cc2ccb9b50a01a1ec59
MaMookMik <mookjrtv3@gmail.com>
2021-06-02 12:02:01 +0700

    test func

--- react/features/polls/components/AbstractPollResults.js
+++ react/features/polls/components/AbstractPollResults.js
@@ -62,7 +62,10 @@ const AbstractPollResults = (Component: AbstractComponent<AbstractProps>) => (pr
         const senderWeights = pollDetails.senderWeights
         console.log('111111senderWeights2:', senderWeights);
 
-        const totalSenderWeight = senderWeights ? 0 : senderWeights.reduce((previous, current) => previous + Number(current.weight), 0)
+        let totalSenderWeight = 0
+        for (const senderWeight of pollDetails.senderWeights) {
+            totalSenderWeight = totalSenderWeight + Number(senderWeight.weight)
+        }
         console.log('111111totalSenderWeight:', totalSenderWeight);
 
         // Getting every voters ID that participates to the poll
