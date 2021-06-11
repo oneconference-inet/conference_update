7b6b96c1075b873bdaa4c50ef86cb4239add6a6c
tlests <tlests@hotmail.com>
2021-06-11 17:35:53 +0700

    test import

--- react/features/base/dialog/components/web/StatelessDialog.js
+++ react/features/base/dialog/components/web/StatelessDialog.js
@@ -7,7 +7,7 @@ import React, { Component } from "react";
 
 import { translate } from "../../../i18n/functions";
 import type { DialogProps } from "../../constants";
-import { disconnect } from "../../../connection";
+// import { disconnect } from "../../../connection";
 // import { createToolbarEvent, sendAnalytics } from "../../../../analytics";
 
 declare var APP: Object;
