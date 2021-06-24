4027b973fc8be7f7c0c6289611d3c9abd04e7a4c
MaMookMik <mookjrtv3@gmail.com>
2021-06-24 10:20:34 +0700

    delete log

--- react/features/remote-video-menu/actions.js
+++ react/features/remote-video-menu/actions.js
@@ -89,8 +89,6 @@ export function muteAllParticipants(exclude: Array<string>) {
 export function endAllParticipants(exclude: Array<string>) {
     return (dispatch: Dispatch<any>, getState: Function) => {
         const state = getState();
-        console.log('1111test', state["features/base/participants"]);
-        console.log('1111test2', participantIds);
         const participantIds = state["features/base/participants"].map(
             (p) => p.id
         );
