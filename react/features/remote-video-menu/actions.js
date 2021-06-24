812d9bb2a1fd84c4e87524314ec749ea9257ddf4
MaMookMik <mookjrtv3@gmail.com>
2021-06-24 21:30:45 +0700

    test log

--- react/features/remote-video-menu/actions.js
+++ react/features/remote-video-menu/actions.js
@@ -91,6 +91,7 @@ export function endAllParticipants(exclude: Array<string>) {
         const state = getState();
         console.log('111123', exclude);
         const localId = getLocalParticipant(state).id;
+        console.log('111124', localId);
         console.log('1111test', state["features/base/participants"]);
         const participantIds = state["features/base/participants"].map(
             (p) => p.id
