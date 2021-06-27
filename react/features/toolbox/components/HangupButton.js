89f8e57a6d8dd65ef13212fdd6348bda48550b8e
MaMookMik <mookjrtv3@gmail.com>
2021-06-27 20:19:19 +0700

    test log

--- react/features/toolbox/components/HangupButton.js
+++ react/features/toolbox/components/HangupButton.js
@@ -62,7 +62,7 @@ class HangupButton extends AbstractHangupButton<Props, *> {
                 const { dispatch, localParticipantId, isModerator } =
                     this.props;
                 if (isModerator) {
-                    // console.log('1111localParticipantId', localParticipantId);
+                    console.log('1111localParticipantId11', localParticipantId);
                     sendAnalytics(createToolbarEvent("endmeeting.pressed"));
                     dispatch(
                         openDialog(EndMeetingDialog, {
