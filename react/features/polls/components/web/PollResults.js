49e6d4c28f42aa90b3ec4633d65706a8bd968578
MaMookMik <mookjrtv3@gmail.com>
2021-07-15 15:21:04 +0700

    poll result

--- react/features/polls/components/web/PollResults.js
+++ react/features/polls/components/web/PollResults.js
@@ -51,9 +51,9 @@ const PollResults = (props: AbstractProps) => {
                             </ul>} */}
                         </li>)
                     )
-                    : answers.map(({ name, percentage, voters, voterCount }, index) =>
+                    : answers.map(({ name, percentage, voterCount }, index) =>
                         (<li key = { index }>
-                            { renderRow(name, percentage, voters.length) }
+                            { renderRow(name, percentage, voterCount) }
                             <div className = 'poll-bar-container'>
                                 <div
                                     className = 'poll-bar'
