23e3c34cd9caf51a4fdfc1cf218c1f43315e7ffc
MaMookMik <mookjrtv3@gmail.com>
2021-07-16 13:28:41 +0700

    poll result voters

--- react/features/polls/components/AbstractPollResults.js
+++ react/features/polls/components/AbstractPollResults.js
@@ -61,8 +61,10 @@ const AbstractPollResults = (Component: AbstractComponent<AbstractProps>) => (pr
         const senderWeights = pollDetails.senderWeights
 
         let totalSenderWeight = 0
+        let totalVoters = 0
         for (const senderWeight of pollDetails.senderWeights) {
             totalSenderWeight = totalSenderWeight + Number(senderWeight.weight)
+            totalVoters = totalVoters + 1
         }
 
         // Getting every voters ID that participates to the poll
@@ -110,7 +112,7 @@ const AbstractPollResults = (Component: AbstractComponent<AbstractProps>) => (pr
                 percentage,
                 voters,
                 voterCount: answer.voters.size,
-                totalVoters: (pollDetails.senderWeights).lenght
+                totalVoters: totalVoters
             };
         });
     }, [ pollDetails.answers ]);
