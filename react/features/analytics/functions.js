b21c93518e1cadf5aa36715938095e42a19ccd34
Thananya Supriyasilp <thananya.su@inet.co.th>
2021-07-20 17:14:44 +0700

    update urlInvite onemail_dga

--- react/features/analytics/functions.js
+++ react/features/analytics/functions.js
@@ -179,7 +179,6 @@ export async function createHandlers({ getState }: { getState: Function }) {
                     );
                 } else if (dataDecode.service == "onemail_dga") {
                     infoConf.setService(dataDecode.service);
-                    infoConf.seturlInvite(keydb.data.urlInvite);
                     keydb = await axios.post(
                         interfaceConfig.DOMAIN_ONEMAIL_DGA + "/checkkey",
                         {
@@ -187,6 +186,7 @@ export async function createHandlers({ getState }: { getState: Function }) {
                             clientname: dataDecode.service,
                         }
                     );
+                    infoConf.seturlInvite(keydb.data.urlInvite);
                 } else {
                     infoConf.setService("oneconference");
                     keydb = await axios.post(
