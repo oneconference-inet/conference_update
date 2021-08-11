d3600015019141e4d5000dbc573bc6a2e56567a1
MaMookMik <mookjrtv3@gmail.com>
2021-08-11 16:24:19 +0700

    fix func check hangup

--- react/features/toolbox/components/HangupButton.js
+++ react/features/toolbox/components/HangupButton.js
@@ -59,7 +59,9 @@ class HangupButton extends AbstractHangupButton<Props, *> {
             if (navigator.product === "ReactNative") {
                 this.props.dispatch(appNavigate(undefined));
             } else {
-                console.log('1111ismod?', isModerator);
+                console.log('1111ismod??', isModerator);
+                console.log('1111userrole', infoConf.getUserRole());
+
                 const { dispatch, localParticipantId, isModerator } =
                     this.props;
                 if (isModerator) {
@@ -126,7 +128,7 @@ export async function _endJoin() {
         const secretKeyEmeeting = interfaceConfig.SECRET_KEY_EMEETING;
         const secretKeyEducation = interfaceConfig.SECRET_KEY_EDUCATION;
         
-        if (isModerator && infoConf.getUserRole == 'moderator') {
+        if (isModerator && infoConf.getUserRole() == 'moderator') {
             infoConf.setIsHostHangup();
         }
 
